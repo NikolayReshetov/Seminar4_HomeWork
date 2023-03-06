f3339edb8752a393fe62ecf831dabeaee69220ed
Nikolay.Reshetov <275480217@mail.ru>
2023-03-07 01:16:15 +0300

    Finish

---
+++ 27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11		82 -> 10		9012 -> 12
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"{n} -> {GetSum(n)}");
+
+int GetSum(int n){
+    if(n<0){
+        n *= (-1);}
+    
+    int count = 0;
+    int m = n;
+    while(m>0){
+        m /=10;
+        count++;}
+    
+    int sum = 0;
+    for(int j=1; j<=count; j++){
+        sum += n%10;
+        n = (n - n%10)/10;}
+    
+    return sum;}
---
+++ 29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+int n = 8;
+int[] array = GetArray(n);
+
+for (int i = 0; i < n-1; i++){
+    Console.Write($"{array[i]}, ");}
+Console.Write($"{array[n-1]}");
+
+Console.WriteLine($" -> [{String.Join(", ", array)}]");
+
+int[] GetArray(int n){
+    int[] result = new int[n];
+    for (int i=0; i<n; i++){
+    result[i] = new Random().Next(50);}
+    return result;}
